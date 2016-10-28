7b48ecf55e4334be572d5c3cfc5a663a7d74ebbf
Alexey Papin <zzheads@gmail.com>
2016-10-28 17:24:40 +0300

    Подключил Java Mail API

--- src/main/java/com/zzheads/CompShop/config/DataConfig.java
+++ src/main/java/com/zzheads/CompShop/config/DataConfig.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
@@ -13,15 +14,15 @@ public class DataConfig {
     public BasicDataSource dataSource() throws URISyntaxException {
 
 //          Use it for deploying
-//
-//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-        String dbUrl = "jdbc:postgresql://localhost:5432/compshop";
-        String username = "zheads";
-        String password = "xela1723014220";
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+//        String dbUrl = "jdbc:postgresql://localhost:5432/compshop";
+//        String username = "zheads";
+//        String password = "xela1723014220";
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
         basicDataSource.setUsername(username);
