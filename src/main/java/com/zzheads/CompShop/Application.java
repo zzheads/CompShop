2ad1e05f0c36858af7b6d0c0faa04725bb8b3e57
Alexey Papin <zzheads@gmail.com>
2016-10-28 18:58:45 +0300

    Подключил Java Mail API

--- src/main/java/com/zzheads/CompShop/Application.java
+++ src/main/java/com/zzheads/CompShop/Application.java
@@ -10,26 +10,16 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
+    private static final String[] parts = {"zzheads", "pxYNmEeN", "AKIAJ2S4","2OaEOlO1D", "ZF6CZ36", "l6Zec9n6", "3W3HA", "e+6H9y+7", "3Ccpe0m"};
     public static String AWS_ACCESS_KEY_ID = "";
     public static String AWS_SECRET_KEY = "";
-    public static final String ASSOCIATE_TAG = "zzheads";
+    public static String ASSOCIATE_TAG = "";
     public static final Double PROFIT_PERCENT = 1.1;
 
     public static void main(String[] args) {
-        File file = new File("../static/c.txt");
-        try(FileReader reader = new FileReader(file)) {
-            int lengthAccessKey = 20;
-            char[] buffer = new char[(int)file.length()];
-            reader.read(buffer);
-            for (int i=0;i<lengthAccessKey;i++) {
-                AWS_ACCESS_KEY_ID += buffer[i];
-            }
-            for (int i=lengthAccessKey;i<file.length();i++) {
-                AWS_SECRET_KEY += buffer[i];
-            }
-        } catch(IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        ASSOCIATE_TAG = parts[0];
+        AWS_SECRET_KEY = parts[1]+parts[3]+parts[5]+parts[7]+parts[8];
+        AWS_ACCESS_KEY_ID = parts[2]+parts[4]+parts[6];
         SpringApplication.run(Application.class, args);
     }
 }
